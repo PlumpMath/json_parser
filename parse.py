56009fb78edbf2f4571c33096e2b5d87d2ce8482
jake <jake@coffshire.com>
2013-07-03 20:56:51 -0500

    Now simple hashes (strings only) are supported.

--- parse.py
+++ parse.py
@@ -18,7 +18,7 @@ def loads(string):
         ret = p.send(c)
 
     if ret is PARSING:
-        raise ValueError("Oops, I wasn't done but your string was.")
+        raise ValueError("Oops, I wasn't done but your JSON string was.")
     return ret
 
 
@@ -27,12 +27,49 @@ def parse_hash():
     """
     hsh = {}
 
-    c = (yield PARSING)
-    if c != "{":
-        raise ValueError("Hashes start with {, fyi. Well, not yours.")
+    # Eat the {
+    yield PARSING
 
     c = (yield PARSING)
-    if c != "}":
-        raise ValueError("Sorry I only support completely useless hashes at the moment.")
+    while c != "}":
+        # json keys are strings, so lets use yet another generator
+        key = PARSING
+        g = parse_string()
+        g.send(None)
+        while key is PARSING:
+            # so this is fun, send a character to be parsed
+            key = g.send(c)
+            # and then go back to the top to get another character
+            c = (yield PARSING)
+
+        if c != ":":
+            raise ValueError("Missing a colon, it looks like this: :")
+
+        # values can be more than just strings, pretending they can't be for now
+        value = PARSING
+        g = parse_string()
+        g.send(None)
+        c = (yield PARSING)
+        while value is PARSING:
+            value = g.send(c)
+            c = (yield PARSING)
+
+        hsh[key] = value
 
     yield hsh
+
+
+def parse_string():
+    """ Generator that slowly parses a string
+    """
+    string = []  # we have to use an array because python strings are immutable
+    c = (yield PARSING)
+    if c != '"':
+        raise ValueError("String start with double quotes, none of this single-quote stuff")
+
+    c = (yield PARSING)
+    while c != '"':
+        string.append(c)
+        c = (yield PARSING)
+
+    yield ''.join(string)
--- test.py
+++ test.py
@@ -10,8 +10,26 @@ class JsonParseTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_hash_happy_path(self):
+    def test_empty_hash(self):
         expected = {}
         string = json.dumps(expected)
         actual = loads(string)
         self.assertDictEqual(expected, actual)
+
+    def test_simple_hash(self):
+        string = '{"key":"value"}'
+        expected = json.loads(string)
+        actual = loads(string)
+        self.assertDictEqual(expected, actual)
+
+    def test_hash_incomplete(self):
+        string = '{"key'
+        self.assertRaises(ValueError, loads, string)
+
+    def test_hash_missing_colon(self):
+        string = '{"key""value"}'
+        self.assertRaises(ValueError, loads, string)
+
+    def test_string_with_tick(self):
+        string = '{"key":\'value\'}'
+        self.assertRaises(ValueError, loads, string)
